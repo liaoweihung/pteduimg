a709edebd954ecee2edc60211dbeb3ff6c9f866e
liaoweihung <liao.weihung@gmail.com>
2026-04-27 00:44:21 +0800

    Refresh app logo

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 // 更新時間戳記，強迫重新整理
-const CACHE_NAME = 'pwa-cache-v202604270930-grip-drag-addacne';
+const CACHE_NAME = 'pwa-cache-v202604271035-round-logo';
 
 // 👇 這裡從 ASSETS 改成了 urlsToCache，這樣 Python 管家才找得到！
 const urlsToCache = [
   './',
   './index.html',
+  './icon.png',
   './img/nasal_4.png',
   './img/acne.png',
   './img/AugmentinSyrup.png',
