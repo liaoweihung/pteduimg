b5a2a321bd19b032361d395f73518826f9780c32
liaoweihung <liao.weihung@gmail.com>
2026-09-04 11:34:22 +0800

    Add acne topic to public card navigation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ?湔???唾?嚗撥餈恍??唳??
-const CACHE_NAME = 'pwa-cache-v202608290302';
+const CACHE_NAME = 'pwa-cache-v202609011128';
 const RUNTIME_CACHE = 'pwa-runtime-v1';
 
 // ?? ?ㄐ敺?ASSETS ?寞?鈭?urlsToCache嚗見 Python 蝞∪振?敺嚗?
@@ -723,6 +723,5 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
-
 
 
